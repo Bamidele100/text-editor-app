dc3f9dc9f5afd64d07b09377e91cde9799438c13
bamidele100 <bamideletalabi01@gmail.com>
2023-03-24 17:47:36 -0300

    edit on the code

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,5 +1,5 @@
-const {  warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
+const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
+const { CacheFirst,StaleWhileRevalidate } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
@@ -17,7 +17,6 @@ const pageCache = new CacheFirst({
       maxAgeSeconds: 30 * 24 * 60 * 60,
     }),
   ],
-  
 });
 
 warmStrategyCache({
@@ -29,21 +28,16 @@ registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
 registerRoute(
-
-   // Here we define the callback function that will filter the requests we want to cache (in this case, JS and CSS files)
-   ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
-   new StaleWhileRevalidate({
-     // Name of the cache storage.
-     cacheName: 'asset-cache',
-     plugins: [
-       // This plugin will cache responses with these headers to a maximum-age of 30 days
-       new CacheableResponsePlugin({
-         statuses: [0, 200],
-       }),
-       new ExpirationPlugin({
-         maxEntries: 60,
-         maxAgeSeconds: 30 * 24 * 60 * 60, //30 Days
-       })
-     ],
-   })
+  // Here we define the callback function that will filter the requests we want to cache (in this case, JS and CSS files)
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  new StaleWhileRevalidate({
+    // Name of the cache storage.
+    cacheName: 'asset-cache',
+    plugins: [
+      // This plugin will cache responses with these headers to a maximum-age of 30 days
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
 );
